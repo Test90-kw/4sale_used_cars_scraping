a7f4f3ab4408a11e39f4ef5c942fe297f190882b
Dataloops-code <dataloopskw.code@gmail.com>
2024-12-22 07:20:06 +0200

    Update SavingOnDrive.py

--- SavingOnDrive.py
+++ SavingOnDrive.py
@@ -13,11 +13,20 @@ class SavingOnDrive:
         self.service = None
 
     def authenticate(self):
-        # Load credentials directly from the JSON content
         creds = Credentials.from_service_account_info(self.credentials_dict, scopes=self.scopes)
         self.service = build('drive', 'v3', credentials=creds)
 
     def create_folder(self, folder_name, parent_folder_id=None):
+        query = f"name = '{folder_name}' and mimeType = 'application/vnd.google-apps.folder'"
+        if parent_folder_id:
+            query += f" and '{parent_folder_id}' in parents"
+
+        results = self.service.files().list(q=query, spaces='drive').execute()
+        folders = results.get('files', [])
+
+        if folders:
+            return folders[0].get('id')
+
         file_metadata = {
             'name': folder_name,
             'mimeType': 'application/vnd.google-apps.folder'
@@ -35,19 +44,7 @@ class SavingOnDrive:
         return file.get('id')
 
     def save_files(self, files):
-        parent_folder_id = '11pG4Jwy1gJUbz7cILT6sfzmLD5f75nqU'  # ID of "Property Scraper Uploads"
-        try:
-            # Check if the folder exists and can be accessed
-            folder = self.service.files().get(fileId=parent_folder_id).execute()
-            print(f"Folder found: {folder['name']}")
-
-        except Exception as e:
-            print(f"Error accessing the folder with ID {parent_folder_id}: {e}")
-            # Optionally, create a new folder if the existing one cannot be accessed
-            print("Attempting to create a new folder...")
-            parent_folder_id = self.create_folder('Cars Scraper Uploads')
-            print(f"New folder created with ID: {parent_folder_id}")
-
+        parent_folder_id = '11pG4Jwy1gJUbz7cILT6sfzmLD5f75nqU'
         yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
         folder_id = self.create_folder(yesterday, parent_folder_id)
 
